bdf2dded72dfd1c4c5710e56b56615f630d7b2db
glassiko <glassiko@gmail.com>
2017-09-25 13:10:35 -0400

    Update EventsBus.cs

--- EventsBus.cs
+++ EventsBus.cs
@@ -6,24 +6,24 @@ using System.Collections.Generic;
 namespace DIY
 {
     /// <summary>
-    ///    Represents a dictionary with events, aggregating publishers and subscribers.
+    ///    Represents dictionary with events, linked between publishers and subscribers.
     /// </summary>
     public class EventsBus
     {
         private ConcurrentDictionary<Type, IList> _events = new ConcurrentDictionary<Type, IList>();
 
-		/// <summary>
-		///    Adds subscriber to a chain of event handlers.
-		/// </summary>
+	/// <summary>
+	///    Adds a given subscriber to a chain of event-handlers.
+	/// </summary>
         public void Subscribe<TEvent>(Action<TEvent> handler)
         {
             var handlers = GetOrAdd<TEvent>();
             handlers.Add(handler);
         }
 
-		/// <summary>
-		///    Triggers all subscribers of a given event.
-		/// </summary>
+	/// <summary>
+	///    Triggers all subscribers of a given event.
+	/// </summary>
         public void Publish<TEvent>(TEvent e)
         {
             var handlers = GetOrAdd<TEvent>();
@@ -33,9 +33,9 @@ namespace DIY
             }
         }
 
-		/// <summary>
-		///    Returns a list of event-handlers.
-		/// </summary>
+	/// <summary>
+	///    Returns a list of event-handlers, subscribed to a given event.
+	/// </summary>
         private List<Action<TEvent>> GetOrAdd<TEvent>()
         {
             var handlers = _events.GetOrAdd(typeof(TEvent), _ => new List<Action<TEvent>>());
